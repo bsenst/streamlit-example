b7ed3c20340e8975f116cb0634b0a0466472e27e
bsenst <benjamin.senst@gmail.com>
2022-12-28 16:02:49 +0100

    Update 1_spacy_streamlit.py

--- pages/1_spacy_streamlit.py
+++ pages/1_spacy_streamlit.py
@@ -1,5 +1,11 @@
 import spacy_streamlit
 
-models = ["en_core_web_sm", "en_core_web_md"]
-default_text = "Sundar Pichai is the CEO of Google."
+models = ["de_core_news_md"]
+
+default_text = """Mit seiner Tagesaufenthalts- und Übernachtungsstätte verfolgt das Harburger Rote Kreuz das Ziel, Not und Hilfebedürftigkeit von obdachlosen Menschen in Harburg sowohl kurzfristig zu lindern als auch langfristig zu beheben.
+
+Das Harburg-Huus bietet tagsüber eine Aufenthaltsmöglichkeit mit bedarfsgerechten Angeboten an sozialen Hilfeleistungen in Form von Obdach, Verpflegung, Hygieneleistungen, medizinischer Grundversorgung, Freizeitangeboten, sozialen Kontakten und einer niedrigschwelligen Sozialberatung.
+
+In den Abend- und Nachtstunden finden obdachlose Menschen hier einen Schutzraum und Schlafmöglichkeiten – bei Bedarf auch zusammen mit ihren Hunden. Es stehen 15 Betten in Ein- bis Vierbettzimmern zur Verfügung. Frauen können in einem gesonderten Schlafraum übernachten.."""
+
 spacy_streamlit.visualize(models, default_text)
