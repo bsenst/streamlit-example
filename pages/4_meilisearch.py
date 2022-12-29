5b5f3b71e5d119e9743165720d920348aad004d5
bsenst <benjamin.senst@gmail.com>
2022-12-29 21:52:31 +0100

    update meilisearch page

--- pages/4_meilisearch.py
+++ pages/4_meilisearch.py
@@ -5,11 +5,42 @@ from streamlit.components.v1 import html
 
 # Define your javascript
 my_js = """
-alert("Hola mundo");
+    const search = instantsearch({
+      indexName: "movies",
+      searchClient: instantMeiliSearch(
+        "http://localhost:7700"
+      )
+      });
+      search.addWidgets([
+        instantsearch.widgets.searchBox({
+          container: "#searchbox"
+        }),
+        instantsearch.widgets.configure({ hitsPerPage: 8 }),
+        instantsearch.widgets.hits({
+          container: "#hits",
+          templates: {
+          item: `
+            <div>
+            <div class="hit-name">
+                  {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+            </div>
+            </div>
+          `
+          }
+        })
+      ]);
+      search.start();
 """
 
 # Wrapt the javascript as html code
-my_html = f"<script>{my_js}</script>"
+my_html = f"""
+    <div class="wrapper">
+      <div id="searchbox" focus></div>
+      <div id="hits"></div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@meilisearch/instant-meilisearch/dist/instant-meilisearch.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4"></script>
+    <script>{my_js}</script>"""
 
 # Execute your app
 st.title("Javascript example")
