03a8d8c9705e910ebb5004ee3abf365330dccdaf
bsenst <benjamin.senst@gmail.com>
2023-01-01 20:58:53 +0100

    update qa

--- pages/6_frage_und_antwort.py
+++ pages/6_frage_und_antwort.py
@@ -1,28 +1,23 @@
-# import json
+import json
 
-# with open("fulltext_subset.json") as infile:
-#     docs = json.load(infile)
+with open("fulltext_subset.json") as infile:
+    docs = json.load(infile)
 
-# from haystack.document_stores import InMemoryDocumentStore
+from haystack.document_stores import InMemoryDocumentStore
+document_store = InMemoryDocumentStore()
+document_store.write_documents(docs)
 
-# document_store = InMemoryDocumentStore()
+from haystack.nodes import TfidfRetriever
+retriever = TfidfRetriever(document_store=document_store)
 
-# document_store.write_documents(docs)
+from haystack.nodes import TransformersReader
+reader = TransformersReader(model_name_or_path="deepset/xlm-roberta-base-squad2", tokenizer="deepset/xlm-roberta-base-squad2", use_gpu=-1)
 
-# from haystack.nodes import TfidfRetriever
+from haystack.pipelines import ExtractiveQAPipeline
+pipe = ExtractiveQAPipeline(reader, retriever)
 
-# retriever = TfidfRetriever(document_store=document_store)
+prediction = pipe.run(
+    query="Welche Dienste bietet das Deutsche Rote Kreuz?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+)
 
-# from haystack.nodes import FARMReader
-
-# reader = FARMReader(model_name_or_path="deepset/xlm-roberta-base-squad2")
-
-# from haystack.pipelines import ExtractiveQAPipeline
-
-# pipe = ExtractiveQAPipeline(reader, retriever)
-
-# prediction = pipe.run(
-#     query="Welche Dienste bietet das Deutsche Rote Kreuz?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
-# )
-
-# print(prediction["answers"])
+print(prediction["answers"])
