64803ebda12b86036aecd948686e2ddc17c3038a
bsenst <benjamin.senst@gmail.com>
2023-01-01 20:18:32 +0100

    update qa

--- pages/6_frage_und_antwort.py
+++ pages/6_frage_und_antwort.py
@@ -2,22 +2,22 @@ import streamlit as st
 
 st.caption("https://www.kdnuggets.com/2021/10/simple-question-answering-web-app-hugging-face-pipelines.html")
 
-# App title and description
-st.title("Frage und Antwort")
+# # App title and description
+# st.title("Frage und Antwort")
 
-# Perform question answering
-# question_answerer = pipeline('question-answering')
+# # Perform question answering
+# # question_answerer = pipeline('question-answering')
 
-answer = ''
-question = st.text_input('', 'Stell deine Frage')
+# answer = ''
+# question = st.text_input('', 'Stell deine Frage')
 
-# if question != '':
-#     answer = question_answerer({
-#         'question': question,
-#         'context': "42 is the answer to everything"
-#     })
+# # if question != '':
+# #     answer = question_answerer({
+# #         'question': question,
+# #         'context': "42 is the answer to everything"
+# #     })
 
-st.write(answer)
+# st.write(answer)
 
 st.caption("https://gist.github.com/demarant/d9e617fb80057692f2f7cddf00013b9b")
 
@@ -44,35 +44,35 @@ st.sidebar.subheader('Options')
 nr_of_answers = st.sidebar.slider('How many relevant answers do you want?', min_value=1, max_value=5)
 
 
-@st.cache(show_spinner=False)
-def get_answers(data):
-    response = requests.post(f"https://haystack-demo-api.deepset.ai/models/1/doc-qa", data=data,
-       headers={
-       "Content-Type": "application/json",
-       "accept": "application/json"
-       }
-    )
-    return response.json()
+# @st.cache(show_spinner=False)
+# def get_answers(data):
+#     response = requests.post(f"https://haystack-demo-api.deepset.ai/models/1/doc-qa", data=data,
+#        headers={
+#        "Content-Type": "application/json",
+#        "accept": "application/json"
+#        }
+#     )
+#     return response.json()
 
-def annotate_context(answer, context):
-    idx = context.find(answer)
-    idx_end = idx + len(answer)
-    annotated_text(context[:idx],(answer,"","#adff2f"),context[idx_end:],)
+# def annotate_context(answer, context):
+#     idx = context.find(answer)
+#     idx_end = idx + len(answer)
+#     annotated_text(context[:idx],(answer,"","#adff2f"),context[idx_end:],)
 
-if question:
-   with st.spinner(text=':crown: Looking for answers...'):
-      results = get_answers(data)
-      if results['results']:
-         for i in range(nr_of_answers):
-            answer = results['results'][0]['answers'][i]['answer']
-            #answer_display.subheader(answer)
-            if answer:
-                context = '...' + results['results'][0]['answers'][i]['context'] + '...'
-                document_name = results['results'][0]['answers'][i]['meta']['name']
-                relevance_pct = round(results['results'][0]['answers'][i]['probability']*100,2)
-                annotate_context(answer, context)
-                '**Relevance:** ', relevance_pct , '**source:** ' , document_name
+# if question:
+#    with st.spinner(text=':crown: Looking for answers...'):
+#       results = get_answers(data)
+#       if results['results']:
+#          for i in range(nr_of_answers):
+#             answer = results['results'][0]['answers'][i]['answer']
+#             #answer_display.subheader(answer)
+#             if answer:
+#                 context = '...' + results['results'][0]['answers'][i]['context'] + '...'
+#                 document_name = results['results'][0]['answers'][i]['meta']['name']
+#                 relevance_pct = round(results['results'][0]['answers'][i]['probability']*100,2)
+#                 annotate_context(answer, context)
+#                 '**Relevance:** ', relevance_pct , '**source:** ' , document_name
 
-if question and st.sidebar.checkbox('Show debug info'):
-    st.subheader('REST API JSON response')
-    st.write(results)
+# if question and st.sidebar.checkbox('Show debug info'):
+#     st.subheader('REST API JSON response')
+#     st.write(results)
