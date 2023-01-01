541c05b4328be5a26dbf582df72e078da0165832
bsenst <benjamin.senst@gmail.com>
2023-01-01 20:48:42 +0100

    update qa

--- pages/6_frage_und_antwort.py
+++ pages/6_frage_und_antwort.py
@@ -1,28 +1,28 @@
-import json
+# import json
 
-with open("fulltext_subset.json") as infile:
-    docs = json.load(infile)
+# with open("fulltext_subset.json") as infile:
+#     docs = json.load(infile)
 
-from haystack.document_stores import InMemoryDocumentStore
+# from haystack.document_stores import InMemoryDocumentStore
 
-document_store = InMemoryDocumentStore()
+# document_store = InMemoryDocumentStore()
 
-document_store.write_documents(docs)
+# document_store.write_documents(docs)
 
-from haystack.nodes import TfidfRetriever
+# from haystack.nodes import TfidfRetriever
 
-retriever = TfidfRetriever(document_store=document_store)
+# retriever = TfidfRetriever(document_store=document_store)
 
-from haystack.nodes import FARMReader
+# from haystack.nodes import FARMReader
 
-reader = FARMReader(model_name_or_path="deepset/xlm-roberta-base-squad2")
+# reader = FARMReader(model_name_or_path="deepset/xlm-roberta-base-squad2")
 
-from haystack.pipelines import ExtractiveQAPipeline
+# from haystack.pipelines import ExtractiveQAPipeline
 
-pipe = ExtractiveQAPipeline(reader, retriever)
+# pipe = ExtractiveQAPipeline(reader, retriever)
 
-prediction = pipe.run(
-    query="Welche Dienste bietet das Deutsche Rote Kreuz?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
-)
+# prediction = pipe.run(
+#     query="Welche Dienste bietet das Deutsche Rote Kreuz?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+# )
 
-print(prediction["answers"])
+# print(prediction["answers"])
