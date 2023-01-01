1f6a684ffa7fa56b6bbb6ab21edc722cbe9eed22
bsenst <benjamin.senst@gmail.com>
2023-01-01 19:15:09 +0100

    update requirements algoliasearch

--- pages/6_frage_und_antwort.py
+++ pages/6_frage_und_antwort.py
@@ -1,21 +1,21 @@
 # https://www.kdnuggets.com/2021/10/simple-question-answering-web-app-hugging-face-pipelines.html
 
 import streamlit as st
-from transformers import pipeline
+# from transformers import pipeline
 
 # App title and description
 st.title("Fragen und Antworten")
 
 # Perform question answering
-question_answerer = pipeline('question-answering')
+# question_answerer = pipeline('question-answering')
 
 answer = ''
 question = st.text_input('Ask a question')
 
-if question != '':
-    answer = question_answerer({
-        'question': question,
-        'context': "42 is the answer to everything"
-    })
+# if question != '':
+#     answer = question_answerer({
+#         'question': question,
+#         'context': "42 is the answer to everything"
+#     })
 
 st.write(answer)
