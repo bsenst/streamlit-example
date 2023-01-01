3a736b885880f0dc4ebf1540d820e1236f740eab
bsenst <benjamin.senst@gmail.com>
2023-01-01 21:33:57 +0100

    update qa

--- pages/6_frage_und_antwort.py
+++ pages/6_frage_und_antwort.py
@@ -1,29 +1,36 @@
 import json
 import streamlit as st
 
-with open("fulltext_subset.json") as infile:
-    docs = json.load(infile)
+st.header("Frage und Antwort")
+st.write("Antworten auf deine Fragen basierend auf einem Teil des Datensatz.")
 
-from haystack.document_stores import InMemoryDocumentStore
-document_store = InMemoryDocumentStore()
-document_store.write_documents(docs)
+with st.spinner(text="In progress..."):
+    with open("fulltext_subset.json") as infile:
+        docs = json.load(infile)
 
-from haystack.nodes import TfidfRetriever
-retriever = TfidfRetriever(document_store=document_store)
+    from haystack.document_stores import InMemoryDocumentStore
+    document_store = InMemoryDocumentStore()
+    document_store.write_documents(docs)
 
-from haystack.nodes import TransformersReader
-reader = TransformersReader(model_name_or_path="deepset/gelectra-base-germanquad-distilled", tokenizer="deepset/gelectra-base-germanquad-distilled", use_gpu=-1)
+    from haystack.nodes import TfidfRetriever
+    retriever = TfidfRetriever(document_store=document_store)
 
-from haystack.pipelines import ExtractiveQAPipeline
-pipe = ExtractiveQAPipeline(reader, retriever)
+    from haystack.nodes import TransformersReader
+    reader = TransformersReader(model_name_or_path="deepset/gelectra-base-germanquad-distilled", tokenizer="deepset/gelectra-base-germanquad-distilled", use_gpu=-1)
+
+    from haystack.pipelines import ExtractiveQAPipeline
+    pipe = ExtractiveQAPipeline(reader, retriever)
 
 question = st.text_input('', 'Welche Dienste bietet das Deutsche Rote Kreuz?')
 
 if question != "":
-    prediction = pipe.run(
-        query=question, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
-    )
+    
+    with st.spinner(text="In progress..."):
 
-    for answer in prediction["answers"]:
-        st.write(answer.answer)
-        st.caption(answer.score)
+        prediction = pipe.run(
+            query=question, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+        )
+
+        for answer in prediction["answers"]:
+            st.write(answer.answer)
+            st.caption(answer.score)
